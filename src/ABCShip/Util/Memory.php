47c8e25874fa1ee92d8159556cfd64e74c422538
Alex Sirotin <alexander.sirotin@gmail.com>
2023-05-12 08:32:54 +0300

    Calc memory

--- src/ABCShip/Util/Memory.php
+++ src/ABCShip/Util/Memory.php
@@ -16,4 +16,12 @@ trait Memory
         $multiplier = strtolower(substr($memoryLimit, -1, 1));
         return ($size[$multiplier] ?? 1) * intval($memoryLimit);
     }
+
+    function isAvailable(int $offset): bool
+    {
+        $memoryLimit = intdiv($this->getMemoryLimit(), 2);
+        $memoryUsed = (memory_get_usage() + $offset);
+
+        return $memoryUsed < $memoryLimit;
+    }
 }
