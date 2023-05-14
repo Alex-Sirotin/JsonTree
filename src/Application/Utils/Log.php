7b2b27438a5835b0b659c36b5aed2d117f169094
Alex Sirotin <alexander.sirotin@gmail.com>
2023-05-15 00:49:48 +0300

    Added logger

---
+++ src/Application/Utils/Log.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace ABCship\Application\Utils;
+
+use Monolog\Level;
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
+
+trait Log
+{
+    public const DEFAULT_LOG_NAME = 'common';
+    public const DEFAULT_LOG_PATH = APPLICATION_PATH . '/logs/';
+    public const DEFAULT_LOG_FILE = self::DEFAULT_LOG_PATH . self::DEFAULT_LOG_NAME . '.log';
+    private static array $logger;
+    public function getLogger(string $file = self::DEFAULT_LOG_FILE, string $name = self::DEFAULT_LOG_NAME, $logLevel = Level::Warning)
+    {
+        if (self::$logger[$name] ?? false) {
+            return self::$logger[$name];
+        }
+        self::$logger[$name] = new Logger($name);
+        self::$logger[$name]->pushHandler(new StreamHandler($file, $logLevel));
+
+        return self::$logger[$name];
+    }
+
+    public function error(string $msg, ?string $name = null): void
+    {
+        $logger = $this->getLogger($name);
+        $logger->error($msg);
+    }
+}
