434fe6f078fd17922cf5bcfb66abf524c72ab92b
Alex Sirotin <alexander.sirotin@gmail.com>
2023-05-15 00:48:34 +0300

    Fix composer

--- Bootstrap.php
+++ Bootstrap.php
@@ -4,10 +4,3 @@ define('APPLICATION_PATH', realpath(dirname(__FILE__)));
 define('APPLICATION_SRC_PATH', realpath(APPLICATION_PATH . '/src'));
 
 include APPLICATION_PATH . '/vendor/autoload.php';
-
-spl_autoload_register(function ($class) {
-    $file = APPLICATION_SRC_PATH . '/' . $class . '.php';
-    if (file_exists($file)) {
-        require_once $file;
-    }
-});
