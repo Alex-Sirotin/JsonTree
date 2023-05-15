50ede269e3db652ea89f360b7789a1a1845cdd1d
Alex Sirotin <alexander.sirotin@gmail.com>
2023-05-15 03:36:21 +0300

    Fix log name and file

--- src/Application/Utils/Log.php
+++ src/Application/Utils/Log.php
@@ -12,8 +12,15 @@ trait Log
     public const DEFAULT_LOG_PATH = APPLICATION_PATH . '/logs/';
     public const DEFAULT_LOG_FILE = self::DEFAULT_LOG_PATH . self::DEFAULT_LOG_NAME . '.log';
     private static array $logger;
-    public function getLogger(string $file = self::DEFAULT_LOG_FILE, string $name = self::DEFAULT_LOG_NAME, $logLevel = Level::Warning)
+    public function getLogger(?string $name, ?string $file = null, Level $logLevel = Level::Warning)
     {
+        if (!$name) {
+            $name = self::DEFAULT_LOG_NAME;
+        }
+        if (!$file) {
+            $file = self::DEFAULT_LOG_FILE;
+        }
+
         if (self::$logger[$name] ?? false) {
             return self::$logger[$name];
         }
